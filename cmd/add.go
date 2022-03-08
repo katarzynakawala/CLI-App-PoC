5315745eb48890886e56597049b9bc3f6041db7f
Katarzyna Kawala <kat.kawala@gmail.com>
2022-03-08 18:52:47 +0100

    addition of two numbers

---
+++ cmd/add.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add the numbers",
+	Long: `Addition of two numbers. The program will also display the result`,
+	Run: func(cmd *cobra.Command, args []string) {
+		num1, _ := strconv.ParseUint(args[0], 10, 32)
+		num2, _ := strconv.ParseUint(args[1], 10, 32)
+		sum := num1 + num2
+		fmt.Println(sum)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
