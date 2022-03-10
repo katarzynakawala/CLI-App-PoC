2a342b0f998befaac64c3ff0acc87802243177ed
Katarzyna Kawala <kat.kawala@gmail.com>
2022-03-10 23:15:37 +0100

    adding substraction command details

--- cmd/minus.go
+++ cmd/minus.go
@@ -6,6 +6,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -13,15 +14,13 @@ import (
 // minusCmd represents the minus command
 var minusCmd = &cobra.Command{
 	Use:   "minus",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Substract two numbers",
+	Long: `Substraction of two numbers. The program will also display the result`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("minus called")
+		num1, _ := strconv.ParseUint(args[0], 10, 32)
+		num2, _ := strconv.ParseUint(args[1], 10, 32)
+		sub := num1 - num2
+		fmt.Println(sub)
 	},
 }
 
